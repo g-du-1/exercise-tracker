7c65df838b5842b51338d7341f941c6206e3d79d
g-du-1 <dudaszeg1@gmail.com>
2024-04-06 10:37:33 +0100

    Add exercises to seeder

--- server/src/main/java/com/gd/exercisetracker/DataLoader.java
+++ server/src/main/java/com/gd/exercisetracker/DataLoader.java
@@ -28,5 +28,10 @@ public class DataLoader implements ApplicationRunner {
 
         // First Pair
         exerciseRepository.save(new Exercise("arch-hangs", "Arch Hangs", "firstPair", "pullUp", 3, 5, 8, false, 90, 90, "https://www.youtube.com/watch?v=C995b3KLXS4&t=7s", "<ul><li>Elbows should stay straight</li><li>Hold it for time/reps</li><li>Start at this <a href=\"https://www.youtube.com/watch?v=HoE-C85ZlCE\">level</a></li><li>Progress towards a 90 degree in your shoulder. Like <a href=\"https://i.ytimg.com/vi/JusddCep6PA/hqdefault.jpg\">this</a></li></ul>"));
+        exerciseRepository.save(new Exercise("bulgarian-split-squats", "Bulgarian Split Squat", "firstPair", "squat", 3, 5, 8, false, 90, 90, "https://www.youtube.com/watch?v=kkdmHTASZg8&t=75s", "<ul><li>Pretend the barbell doesn't exist.</li><li>You can progress this further by elevating both your legs</li></ul>"));
+        
+        // Second Pair
+        exerciseRepository.save(new Exercise("parallel-bar-support-hold", "Parallel Bar Support Hold", "secondPair", "dip", 3, 60, 60, true, 90, 90, "https://antranik.org/wp-content/uploads/2014/01/antranik-holding-support-hold-on-parallel-bars.jpg", "<ul><li>Work up to 3 sets of 1 minute holds for this progression</li><li>If you can't hold yourself up at all, consider using bands or your feet to assist you until you gain the necessary strength.</li></ul>"));
+        exerciseRepository.save(new Exercise("negative-hamstring-slide", "Negative Hamstring Slide", "secondPair", "hinge", 3, 5, 8, false, 90, 90, "https://www.youtube.com/watch?v=OY8Jci5KJdM", "<ul><li>Start in the top position of a glute bridge, slide legs out under control until your butt touches the ground/knees are extended.</li><li>Return to starting position and repeat for desired reps.</li></ul>"));
     }
 }
