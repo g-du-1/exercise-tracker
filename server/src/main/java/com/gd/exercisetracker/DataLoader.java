a2082b7ca883436e6c8f404938d82e49fe8c1efd
g-du-1 <dudaszeg1@gmail.com>
2024-04-06 10:00:33 +0100

    Add data loader

---
+++ server/src/main/java/com/gd/exercisetracker/DataLoader.java
@@ -0,0 +1,25 @@
+package com.gd.exercisetracker;
+
+import com.gd.exercisetracker.exercise.Exercise;
+import com.gd.exercisetracker.exercise.ExerciseRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+// Temp Data Loader that will seed the DB on application start
+// Comment out @Component to disable
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    private final ExerciseRepository exerciseRepository;
+
+    public DataLoader(ExerciseRepository exerciseRepository) {
+        this.exerciseRepository = exerciseRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        exerciseRepository.save(new Exercise("arch-hangs", "Arch Hangs", "firstPair", "pullUp", 3, 5, 8, false, 90, 90, "https://www.youtube.com/watch?v=C995b3KLXS4&t=7s", "<ul><li>Do as many reps as you want</li></ul>"));
+    }
+}
