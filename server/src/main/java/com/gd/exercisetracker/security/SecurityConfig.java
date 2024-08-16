3d14e954db6ecb87f65af43c364fa8cb7a083dfb
g-du-1 <dudaszeg1@gmail.com>
2024-08-16 16:19:07 +0100

    Add multi user dev setup

--- server/src/main/java/com/gd/exercisetracker/security/SecurityConfig.java
+++ server/src/main/java/com/gd/exercisetracker/security/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,12 +19,28 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request ->
-                request
-                        .requestMatchers("/public/**").permitAll()
-                        .anyRequest().authenticated());
+        http.authorizeHttpRequests(request -> request.requestMatchers("/public/**").permitAll().anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        if (!manager.userExists("user1")) {
+            UserDetails user1 = User.withUsername("user1").password("{noop}password1").roles("USER").build();
+
+            manager.createUser(user1);
+        }
+
+        if (!manager.userExists("admin")) {
+            UserDetails adminUser = User.withUsername("admin").password("{noop}adminPass").roles("ADMIN").build();
+
+            manager.createUser(adminUser);
+        }
+
+        return manager;
     }
 }
