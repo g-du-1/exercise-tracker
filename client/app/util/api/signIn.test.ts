67ff440f585d00c81c4fb548a8c4eb5ab9bc789e
g-du-1 <dudaszeg1@gmail.com>
2024-09-13 22:21:34 +0100

    Implement sign in util

---
+++ client/app/util/api/signIn.test.ts
@@ -0,0 +1,89 @@
+import { fetchWithAuth } from "../fetchWithAuth";
+import { signIn } from "./signIn";
+import { localStorageMock } from "../../tests/localStorageMock";
+
+jest.mock("../fetchWithAuth", () => ({
+  fetchWithAuth: jest.fn(),
+}));
+
+describe("signIn", () => {
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+
+    Object.defineProperty(window, "localStorage", {
+      value: localStorageMock,
+    });
+
+    localStorage.setItem("JWT_TOKEN", "");
+
+    (fetchWithAuth as jest.Mock).mockImplementation(() =>
+      Promise.resolve({
+        status: 200,
+        json: () =>
+          Promise.resolve({
+            jwtToken: "token",
+            username: "user1",
+            roles: ["ROLE_USER"],
+          }),
+      }),
+    );
+
+    process.env = { ...OLD_ENV };
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV;
+  });
+
+  it("calls fetchWithAuth with endpoint set in env and maps status", async () => {
+    process.env.NEXT_PUBLIC_API_SIGN_IN_ENDPOINT = "sign-in-endpoint";
+
+    const result = await signIn("testUser", "testPassword");
+
+    expect(fetchWithAuth).toHaveBeenNthCalledWith(1, "sign-in-endpoint", {
+      method: "POST",
+      body: '{"username":"testUser","password":"testPassword"}',
+    });
+
+    expect(result).toStrictEqual({ status: 200 });
+  });
+
+  it("calls fetchWithAuth with default endpoint if none set in env", async () => {
+    await signIn("testUser", "testPassword");
+
+    expect(fetchWithAuth).toHaveBeenNthCalledWith(1, "/auth/public/signin", {
+      method: "POST",
+      body: '{"username":"testUser","password":"testPassword"}',
+    });
+  });
+
+  it("sets the jwt in localstorage on successful login", async () => {
+    await signIn("testUser", "testPassword");
+
+    const mockJwtInStorage = localStorageMock.getItem("JWT_TOKEN");
+
+    expect(mockJwtInStorage).toEqual("token");
+  });
+
+  it("does not set the jwt on unsuccessful login", async () => {
+    (fetchWithAuth as jest.Mock).mockImplementation(() =>
+      Promise.resolve({
+        status: 401,
+        json: () =>
+          Promise.resolve({
+            message: "Bad credentials",
+            status: false,
+          }),
+      }),
+    );
+
+    await signIn("testUser", "testPassword");
+
+    const mockJwtInStorage = localStorageMock.getItem("JWT_TOKEN");
+
+    expect(mockJwtInStorage).toEqual("");
+  });
+});
--- client/app/util/api/signIn.ts
+++ client/app/util/api/signIn.ts
@@ -1,5 +1,24 @@
+import { fetchWithAuth } from "../fetchWithAuth";
+
 export const signIn = async (username: string, password: string) => {
+  const response = await fetchWithAuth(
+    process.env.NEXT_PUBLIC_API_SIGN_IN_ENDPOINT || "/auth/public/signin",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    },
+  );
+
+  const data = await response.json();
+
+  if (response.status === 200 && data.jwtToken) {
+    localStorage.setItem("JWT_TOKEN", data.jwtToken);
+  }
+
   return {
-    status: 200,
+    status: response.status,
   };
 };
