ed2166f6fa0620ec90720e30e3c029029a24f3c6
g-du-1 <dudaszeg1@gmail.com>
2024-09-21 21:02:30 +0000

    Update CI for vitest

--- client/vitest.config.mjs
+++ client/vitest.config.mjs
@@ -4,6 +4,24 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      reporter: ["text", "json", "html"],
+      all: true,
+      include: ["app/**/*.{ts,tsx,js,jsx}"],
+      exclude: [
+        "node_modules/",
+        "**/types.ts",
+        "**/theme.ts",
+        "**/layout.tsx",
+        "**/constants.ts",
+      ],
+      thresholds: {
+        lines: 80,
+        functions: 100,
+        branches: 80,
+        statements: 80,
+      },
+    },
     globals: true,
     environment: "jsdom",
     setupFiles: ["./setup-vitest.ts"],
