76feecb463bb0bbb7e02b235fdc6b60aa5d2193a
Gergo Dudaszeg <dudaszeg1@gmail.com>
2025-07-16 17:32:42 +0100

    Turn on feature flag and fix tests

--- client/app/settings/settingsPage.test.tsx
+++ client/app/settings/settingsPage.test.tsx
@@ -65,6 +65,8 @@ const wrapper = ({ children }: { children: React.ReactNode }) => {
 
 describe("SettingsPage", () => {
   beforeEach(() => {
+    process.env.NEXT_PUBLIC_ENABLE_API_CONNECTION = "true";
+
     (useRouter as Mock).mockImplementation(() => ({
       push: mockPush,
     }));
@@ -106,7 +108,51 @@ describe("SettingsPage", () => {
   it("adds a user exercise and disables add", async () => {
     nock("http://localhost:3000")
       .get("/api/v1/exercises")
-      .reply(200, mockExercises);
+      .reply(200, mockExercises)
+      .get("/api/v1/user-exercises")
+      .reply(200, [])
+      .post("/api/v1/user-exercises/save")
+      .reply(200, {
+        id: 6,
+        exercise: {
+          id: 13,
+          key: "diamond-pushup",
+          name: "Diamond Pushup",
+          category: "THIRD_PAIR",
+          type: "PUSH_UP",
+          targetSets: 3,
+          targetRepsMin: 5,
+          targetRepsMax: 8,
+          targetRest: 90,
+          additionalRest: 90,
+          mediaLink: "https://www.youtube.com/watch?v=J0DnG1_S92I",
+          comments:
+            "<ul><li>Put your hands close together so the thumbs and index fingers touch, then perform a pushup</li><li>If this is too difficult or feels uncomfortable, put your hands just a bit closer than in a normal pushup. Work on moving the hands closer together over time until you reach diamond pushups</li></ul>",
+          duration: false,
+        },
+      })
+      .get("/api/v1/user-exercises")
+      .reply(200, [
+        {
+          id: 6,
+          exercise: {
+            id: 13,
+            key: "diamond-pushup",
+            name: "Diamond Pushup",
+            category: "THIRD_PAIR",
+            type: "PUSH_UP",
+            targetSets: 3,
+            targetRepsMin: 5,
+            targetRepsMax: 8,
+            targetRest: 90,
+            additionalRest: 90,
+            mediaLink: "https://www.youtube.com/watch?v=J0DnG1_S92I",
+            comments:
+              "<ul><li>Put your hands close together so the thumbs and index fingers touch, then perform a pushup</li><li>If this is too difficult or feels uncomfortable, put your hands just a bit closer than in a normal pushup. Work on moving the hands closer together over time until you reach diamond pushups</li></ul>",
+            duration: false,
+          },
+        },
+      ]);
 
     render(<SettingsPage />, { wrapper });
 
@@ -114,10 +160,80 @@ describe("SettingsPage", () => {
       expect(screen.getByText("Diamond Pushup")).toBeInTheDocument();
     });
 
-    await fireEvent.click(screen.getByLabelText("Add Diamond Pushup"));
+    await waitFor(async () => {
+      await fireEvent.click(screen.getByLabelText("Add Diamond Pushup"));
+    });
 
     await waitFor(() => {
       expect(screen.getByLabelText("Add Diamond Pushup")).toBeDisabled();
     });
   });
+
+  // it("deletes all exercises for user", async () => {
+  //   nock("http://localhost:3000")
+  //     .post("/api/v1/user-exercises/save")
+  //     .reply(200, {
+  //       id: 6,
+  //       exercise: {
+  //         id: 21,
+  //         key: "reverse-hyperextension",
+  //         name: "Reverse Hyperextension",
+  //         category: "CORE_TRIPLET",
+  //         type: "EXTENSION",
+  //         targetSets: 3,
+  //         targetRepsMin: 8,
+  //         targetRepsMax: 12,
+  //         targetRest: 60,
+  //         additionalRest: 60,
+  //         mediaLink: "https://www.youtube.com/watch?v=ZeRsNzFcQLQ&",
+  //         comments: "<ul><li>Keep your butt tucked</li></ul>",
+  //         duration: false,
+  //       },
+  //     });
+  //
+  //   nock("http://localhost:3000")
+  //     .delete("/api/v1/user-exercises/all")
+  //     .reply(200, { message: "Deleted all exercises" });
+  //
+  //   nock("http://localhost:3000")
+  //     .get("/api/v1/user-exercises")
+  //     .reply(200, mockUserExercises);
+  //
+  //   nock("http://localhost:3000").get("/api/v1/user-exercises").reply(200, []);
+  //
+  //   nock("http://localhost:3000")
+  //     .get("/api/v1/exercises")
+  //     .reply(200, mockExercises);
+  //
+  //   render(<SettingsPage />, { wrapper });
+  //
+  //   await waitFor(() => {
+  //     expect(
+  //       screen.getByLabelText("Add Reverse Hyperextension"),
+  //     ).toBeInTheDocument();
+  //   });
+  //
+  //   await waitFor(() => {
+  //     expect(screen.getByLabelText("Add Reverse Hyperextension")).toBeEnabled();
+  //     expect(screen.getByLabelText("Add Diamond Pushup")).toBeEnabled();
+  //   });
+  //
+  //   await act(async () => {
+  //     fireEvent.click(screen.getByLabelText("Add Reverse Hyperextension"));
+  //   });
+  //
+  //   await waitFor(() => {
+  //     expect(
+  //       screen.getByLabelText("Add Reverse Hyperextension"),
+  //     ).toBeDisabled();
+  //   });
+  //
+  //   await act(async () => {
+  //     fireEvent.click(screen.getByLabelText("Delete All Of My Exercises"));
+  //   });
+  //
+  //   await waitFor(() => {
+  //     expect(screen.getByLabelText("Add Reverse Hyperextension")).toBeEnabled();
+  //   });
+  // });
 });
