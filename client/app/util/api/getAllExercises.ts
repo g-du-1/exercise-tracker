4db38b3c80b4297720a32f0ea24f95de08465320
Gergo Dudaszeg <dudaszeg1@gmail.com>
2025-07-16 21:40:41 +0100

    Remove redundant code

--- client/app/util/api/getAllExercises.ts
+++ client/app/util/api/getAllExercises.ts
@@ -6,9 +6,5 @@ export const getAllExercises = async (): Promise<Exercise[]> => {
     process.env.NEXT_PUBLIC_GET_ALL_EXERCISES_ENDPOINT || "/exercises",
   );
 
-  if (resp.status !== 200) {
-    throw new Error("Failed to get all exercises.");
-  }
-
   return resp.json();
 };
