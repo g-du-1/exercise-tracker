1b3e2247d79a2543c08b0217d7ede3e289a89d15
Gergo Dudaszeg <dudaszeg1@gmail.com>
2025-07-20 17:14:43 +0100

    Add audit fields

--- server/src/main/java/com/gd/exercisetracker/exercise/Exercise.java
+++ server/src/main/java/com/gd/exercisetracker/exercise/Exercise.java
@@ -5,6 +5,11 @@ import com.gd.exercisetracker.exercise.enums.ExerciseType;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -30,4 +35,13 @@ public class Exercise {
 
     @Column(columnDefinition = "TEXT")
     private String comments;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+    
+    @CreatedBy
+    private String createdBy;
 }
